3aaa814537132f38a51719655e781fcb12101453
Hugo Benavides <the.another.gerf@gmail.com>
2023-08-18 15:53:19 -0600

    Iniciando con React en SPA

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './assets/css/base/base.css';
+import './assets/css/componentes/card.css'
+import Home from "./pages/Home.jsx"
+import Sobre from "./pages/Sobre.jsx"
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Sobre />
+
+    </>
+  );
+}
+
+export default App;
