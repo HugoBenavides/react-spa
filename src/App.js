d6fcbd8aaab49a2e8595acc096f436ca64ed1e53
Hugo Benavides <the.another.gerf@gmail.com>
2023-08-22 15:44:32 -0600

    agregando subcategorias, depurar código u optimizar como ejercicio

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Sobre from "./pages/Sobre.jsx"
 import Page404 from './pages/Page404';
 import Header from './components/Header';
 import Post from './pages/Post';
+import Categoria from './pages/Categorias';
 
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/sobre" element={<Sobre />}/>
         <Route path="/posts/:id" element={<Post />}/>
+        <Route path="/categorias/:id/*" element={<Categoria />} />
         <Route path="*" element={<Page404 />}/>
       </Routes>
     </Router>
