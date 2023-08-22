6b0cb9ec24297859e10492aa0c322da96595770f
Hugo Benavides <the.another.gerf@gmail.com>
2023-08-22 15:11:34 -0600

    agregando los compenentes de categoria a <Home />

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import ListPosts from "../components/ListPosts" // se importa para poder agregar la informacionde API REST que tenemos guarda en loscalhost:5000
+import ListCategories from "../components/ListCategories"
+
 
 const Home = () => {
   return (
@@ -6,6 +8,7 @@ const Home = () => {
       <div className="container">
         <h2 className="title-page">Pet noticias</h2>
       </div>
+        <ListCategories />
         <ListPosts  url={"/posts"} />
     </main>
   )
